867adfffcf61593d39a53feb81291fd1a869d645
Isable-maye <297375266@qq.com>
2020-10-27 16:15:43 +0800

    first app的实现

---
+++ app/src/main/java/com/swufe/mobl_devlp_cls_zf_jm/DSecondFragment_first.java
@@ -0,0 +1,34 @@
+package com.swufe.mobl_devlp_cls_zf_jm;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class DSecondFragment_first extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.d_fragment_second_first, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(DSecondFragment_first.this)
+                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+            }
+        });
+    }
+}
